649618811117270b090bce02d14e071e0d82b101
Gabriel Luciano <Gluciano@Gabriels-MacBook-Pro.local>
2018-05-21 22:29:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+// CHALLENGE 1
+var num
+function double(){
+if(num>2){
+return num*2
+}
+} 
+//run:
+double(num=3)
+//result: 
+6
+
+//CHALLENGE 2
+var newNum
+function double(){
+if(newNum<2){
+return newNum*2
+}
+else{
+return newNum}
+} 
+//run:
+double(newNum=1)
+//result:
+2
+
+//CHALLENGE 3
+var num
+function evenCheck(){
+    if(num % 2 == 0){
+        return true;}
+    
+        else{
+            return false}
+}
+//run:
+evenCheck(num=3)
+//result:
+false
+
+//CHALLENGE 4
+var checkMe = [1,2,3]
+function checkIt(){
+    for(var i=0;i<checkMe.length;i++){
+        if(checkMe.length>5){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+
+//CHALLENGE 5
+var isDivisibleBy4
+function isIt(){
+    if(isDivisibleBy4 % 4 == 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+//run:
+isIt(isDivisibleBy4=16)
+
+//result:
+true
+
+//Challenge 6
+
+while (i < 10) {
+   i+2
+   i+3
+}
+
+
+//Challenge 7
+
+  
+
+
